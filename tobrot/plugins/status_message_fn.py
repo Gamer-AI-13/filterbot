fdf2c8ce22d3fc3a925d2351486064f908a8df18
Gamer-AI-13 <67921075+Gamer-AI-13@users.noreply.github.com>
2020-09-12 23:18:42 +0530

    Update status_message_fn.py

--- tobrot/plugins/status_message_fn.py
+++ tobrot/plugins/status_message_fn.py
@@ -68,7 +68,7 @@ async def status_message_f(client, message):
             msg += "\n\n"
         LOGGER.info(msg)
         if msg == "":
-            msg = "🤷‍♂️ No Active, Queued or Paused TORRENTs"
+            msg = "🤷‍♂️ No Active, Queued or Paused URL Downloads "
         await message.reply_text(msg, quote=True)
 
 
